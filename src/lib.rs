99d2cf337105af2041292ea3be0f05584579932d
bendeez <blaziken@MSI>
2024-12-27 21:48:13 -0600

    implemented storage

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,36 @@
+use std::fmt;
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Result, Seek, Write};
 use std::path::Path;
 
 const PAGE_SIZE: usize = 8192;
 
+#[derive(Debug, Clone)]
 struct PageHeaderOffsets {
     pub id: (usize, usize),
     pub lower: (usize, usize),
     pub higher: (usize, usize),
 }
 
+#[derive(Debug, Clone)]
+struct PageHeader {
+    pub id: u16,
+    pub lower: u16,
+    pub higher: u16,
+}
+
 enum DataField {
     Text(String),
     Integer(u16),
+}
+
+impl DataField {
+    pub fn to_int(&self) -> u16 {
+        match self {
+            DataField::Integer(_) => 1,
+            DataField::Text(_) => 2,
+        }
+    }
 }
 
 pub struct Storage {
@@ -30,12 +48,7 @@ impl Storage {
         }
     }
 
-    pub fn insert_data(
-        &self,
-        file_path: &str,
-        data_fields: &Vec<DataField>,
-        data_len: u16,
-    ) -> Result<()> {
+    pub fn insert_data(&self, file_path: &str, data_fields: &Vec<DataField>) -> Result<()> {
         let path = Path::new(file_path);
         let (mut file, mut page) = if path.exists() {
             let mut file = OpenOptions::new().read(true).write(true).open(file_path)?;
@@ -47,7 +60,7 @@ impl Storage {
             let page = self.create_page()?;
             (file, page)
         };
-        self.write_page(&mut page, data_fields, data_len);
+        self.write_page(&mut page, data_fields);
         file.seek(std::io::SeekFrom::Start(0))?;
         file.write_all(&page)?;
 
@@ -58,64 +71,82 @@ impl Storage {
         [0u8; PAGE_SIZE].to_vec()
     }
 
-    fn write_metadata(&self, page: &mut Vec<u8>) {
-        let id = 1 as u16;
+    fn write_metadata(&self, page: &mut Vec<u8>, page_header: &PageHeader) {
+        let id = page_header.id;
         let page_header_offsets = &self.page_header_offsets;
         page[page_header_offsets.id.0..page_header_offsets.id.1].copy_from_slice(&id.to_le_bytes());
-        let lower = 6 as u16;
+        let lower = page_header.lower;
         page[page_header_offsets.lower.0..page_header_offsets.lower.1]
             .copy_from_slice(&lower.to_le_bytes());
-        let higher = PAGE_SIZE;
+        let higher = page_header.higher;
         page[page_header_offsets.higher.0..page_header_offsets.higher.1]
             .copy_from_slice(&higher.to_le_bytes());
     }
 
-    fn write_page(&self, page: &mut Vec<u8>, data_fields: &Vec<DataField>, data_len: u16) {
+    fn read_metadata(&self, page: &Vec<u8>) -> PageHeader {
         let page_header_offsets = &self.page_header_offsets;
-        let mut lower = u16::from_le_bytes(
+        let id = u16::from_le_bytes(
+            page[page_header_offsets.id.0..page_header_offsets.id.1]
+                .try_into()
+                .unwrap(),
+        );
+        let lower = u16::from_le_bytes(
             page[page_header_offsets.lower.0..page_header_offsets.lower.1]
                 .try_into()
                 .unwrap(),
         );
-        let mut higher = u16::from_le_bytes(
+        let higher = u16::from_le_bytes(
             page[page_header_offsets.higher.0..page_header_offsets.higher.1]
                 .try_into()
                 .unwrap(),
         );
-        higher -= data_len;
-        let pointer_offset = lower as usize;
-        // adds a pointer thats value is the: original higher value - data_len
-        page[pointer_offset..pointer_offset + 2].copy_from_slice(&higher.to_le_bytes());
-        // lower value updated due to added pointer
-        lower += 2;
-        let mut data_offset = higher as usize;
+        PageHeader { id, lower, higher }
+    }
+
+    fn write_page(&self, page: &mut Vec<u8>, data_fields: &Vec<DataField>) {
+        let mut row = Vec::new();
+        let mut data = Vec::new();
+        let mut data_len = 0 as u16;
+        row.extend_from_slice(&(data_fields.len() as u16).to_le_bytes());
+        data_len += 2;
         for field in data_fields {
+            row.extend_from_slice(&field.to_int().to_le_bytes());
+            data_len += 2;
             match field {
                 DataField::Text(text) => {
                     let text_to_bytes = text.as_bytes();
                     let text_len = text_to_bytes.len() as u16;
-                    page[data_offset..data_offset + 2].copy_from_slice(&text_len.to_le_bytes());
-                    data_offset += 2;
-                    page[data_offset..data_offset + text_len as usize]
-                        .copy_from_slice(text_to_bytes);
-                    data_offset += text_len as usize;
+                    data.extend_from_slice(&text_len.to_le_bytes());
+                    data_len += 2;
+                    data.extend_from_slice(text_to_bytes);
+                    data_len += text_len;
                 }
                 DataField::Integer(int) => {
-                    page[data_offset..data_offset + 2].copy_from_slice(&int.to_le_bytes());
-                    data_offset += 2;
+                    data.extend_from_slice(&int.to_le_bytes());
+                    data_len += 2;
                 }
             }
         }
-        // modifies lower and higher values within the page
-        page[page_header_offsets.lower.0..page_header_offsets.lower.1]
-            .copy_from_slice(&lower.to_le_bytes());
-        page[page_header_offsets.higher.0..page_header_offsets.higher.1]
-            .copy_from_slice(&higher.to_le_bytes());
+        row.extend_from_slice(&data);
+        let mut page_header = self.read_metadata(&page);
+        page_header.higher -= data_len;
+        let data_offset = page_header.higher;
+        page[data_offset as usize..(data_offset + data_len) as usize].copy_from_slice(&row);
+        let pointer_offset = page_header.lower;
+        page[pointer_offset as usize..pointer_offset as usize + 2]
+            .copy_from_slice(&data_offset.to_le_bytes());
+        page_header.lower += 2;
+        self.write_metadata(page, &page_header);
     }
 
     fn create_page(&self) -> Result<Vec<u8>> {
         let mut page = self.default_page();
-        self.write_metadata(&mut page);
+        let page_header = PageHeader {
+            id: 1,
+            lower: 6,
+            higher: PAGE_SIZE as u16,
+        };
+        self.write_metadata(&mut page, &page_header);
         Ok(page)
     }
 }
@@ -123,4 +154,15 @@ impl Storage {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_create_page() {
+        let storage = Storage::new();
+        let mut page = storage.create_page().unwrap();
+        let data_fields = vec![DataField::Text("data".to_string()), DataField::Integer(10)];
+        storage.write_page(&mut page, &data_fields);
+        println!("{:?}", page);
+        let page_headers = storage.read_metadata(&page);
+        println!("{:?}", page_headers);
+    }
 }
